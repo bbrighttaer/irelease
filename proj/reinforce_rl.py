1d8b90b247faa45f3a34e2a6b9fea4761f6214c0
bbrighttaer <brighteragyemang@gmail.com>
2020-05-11 21:48:46 +0800

    REINFORCE batching passed

--- proj/reinforce_rl.py
+++ proj/reinforce_rl.py
@@ -328,7 +328,7 @@ def main(flags):
 
 
 def default_hparams(args):
-    return {'d_model': 15,
+    return {'d_model': 1500,
             'dropout': 0.1,
             'monte_carlo_N': 10,
             'gamma': 0.99,
@@ -348,7 +348,7 @@ def default_hparams(args):
                               'optimizer__global__lr': 0.001, },
             'agent_params': {'unit_type': 'gru',
                              'num_layers': 2,
-                             'stack_width': 15,
+                             'stack_width': 1500,
                              'stack_depth': 200,
                              'optimizer': 'adadelta',
                              'optimizer__global__weight_decay': 0.00005,
