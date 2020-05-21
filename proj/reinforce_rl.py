5315ba5210ecd4570642618c33df739cc5b47ff9
bbrighttaer <brighteragyemang@gmail.com>
2020-05-21 18:38:59 +0000

    d_model correction

--- proj/reinforce_rl.py
+++ proj/reinforce_rl.py
@@ -360,7 +360,7 @@ def main(flags):
 
 
 def default_hparams(args):
-    return {'d_model': 15,
+    return {'d_model': 1500,
             'dropout': 0.0,
             'monte_carlo_N': 5,
             'gamma': 0.97,
@@ -378,7 +378,7 @@ def default_hparams(args):
                               'optimizer__global__lr': 0.001, },
             'agent_params': {'unit_type': 'gru',
                              'num_layers': 1,
-                             'stack_width': 15,
+                             'stack_width': 1500,
                              'stack_depth': 200,
                              'optimizer': 'adadelta',
                              # 'optimizer__global__weight_decay': 0.00005,
